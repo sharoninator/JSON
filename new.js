166a22b2f1aa894d2aee29bcba90dca3bb1df231
Gorgamite <35180531+Gorgamite@users.noreply.github.com>
2018-01-06 16:22:50 -0800

    Update new.js

--- new.js
+++ new.js
@@ -14,7 +14,7 @@ var y;
  var y = (817/180)*(90-(parseFloat(data.iss_position.latitude))) ;
                               var x = (1520 / 360) * (180+(parseFloat(data.iss_position.longitude)) - 15);
                               fill(255,78,0);
-   coords.innerHTML = "Latitude: " + data.iss_position.latitude <br> Longitude: " + data.iss_position.longitude;
+   coords.innerHTML = "Latitude: " + data.iss_position.latitude <br> Longitude: "  + data.iss_position.longitude;
       background(bg); 
 
     ellipse(x,y,30,30);
