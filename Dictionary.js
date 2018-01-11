1fa9c68c46d388c7859f5e0009abad83e36d2c1a
Gorgamite <35180531+Gorgamite@users.noreply.github.com>
2018-01-11 14:52:26 -0800

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -1,1 +1,6 @@
-
+for(var i =0;i<2000;i++){
+$.get( "https://qrng.anu.edu.au/API/jsonI.php?length= + i + "&type=uint8&#8217", function( data ) {
+           var num = data[0];                 
+});
+  createP(data.success);
+}
