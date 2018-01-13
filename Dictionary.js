ab4e138d28c7e39d2c389868a5acbc6a90d232d0
Gorgamite <35180531+Gorgamite@users.noreply.github.com>
2018-01-12 21:10:41 -0800

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -2,8 +2,8 @@ var mic
 var bg;
 var slider;
 function setup(){
-     bg = loadImage("faceBank.jpg");
-  createCanvas(800,800);
+     bg = loadImage("http://jzool-prd.s3.amazonaws.com/img/17179/m.jpg?1360320930");
+  createCanvas(700,700);
   mic = new p5.AudioIn();
   mic.start();
   createP("Microphone sensitivity");
@@ -15,9 +15,9 @@ function setup(){
   var vol = map(mic.getLevel(),0,1,1,slider.value());
   if(vol > 90){
     vol = 90;
-      ellipse(400,440,230,90);
+      ellipse(300,385,230,90);
   } else{
-      ellipse(400,440,230,vol);
+      ellipse(300,385,230,vol);
   }
 
 
