505cf1bc696477f39ac68ba1ba91e65c2480aa27
Gorgamite <35180531+Gorgamite@users.noreply.github.com>
2018-01-17 15:27:24 -0800

    Update new.js

--- randomProjects/Test/new.js
+++ randomProjects/Test/new.js
@@ -1,23 +1,33 @@
- var bg;
-  var x;
-var y;
-    function setup() {
+var bird;
+function setup(){
+  createCanvas(600,600);
+bird = new Bird();
 
-    bg = loadImage("https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Simple_world_map.svg/2000px-Simple_world_map.svg.png");
-   bg.resize(1520, 817);
-    createCanvas(1520, 817);
-   
+}
+function draw(){
+  background(51);
+
+bird.moveBall();
   }
-  function draw() {
-   
-  $.get( "http://api.open-notify.org/iss-now.json", function( data ) {
- var y = (817/180)*(90-(parseFloat(data.iss_position.latitude))) ;
-                              var x = (1520 / 360) * (180+(parseFloat(data.iss_position.longitude)) - 15);
-                              fill(255,78,0);
-   coords.innerHTML = "Latitude: " + data.iss_position.latitude + " Longitude: "  + data.iss_position.longitude;
-      background(bg); 
-    ellipse(x,y,30,30);
+
+
+//Bird object //
+function Bird(){
+  this.y = 403;
+  this.x = 43;
+  this.d=30
+ this.moveBall = function(){
+    this.x+=10;
   
-                              
-});
-  } 
+    ellipse(this.x,this.y,this.d);
+    if(this.x < 0   || this.x > width){
+      this.x = this.x - 30
+      this.x = this.x  *-1;
+    }
+        if(this.y <  0|| this.y > height){
+      this.y = this.y* -1;
+    } 
+}
+
+}
+
