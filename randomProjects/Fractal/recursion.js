e5ec294e1534480ec63c6e28c3fc29e5aa716d6d
Gorgamite <35180531+Gorgamite@users.noreply.github.com>
2018-01-19 21:24:40 -0800

    Update recursion.js

--- randomProjects/Fractal/recursion.js
+++ randomProjects/Fractal/recursion.js
@@ -1,20 +1,23 @@
-
 function draw(){
   createCanvas(800,800);
   background(51);
   stroke(255);
-noFill();
+  noFill();
   drawCircle(300,200,300);
-createP("1");
-}
-function drawCircle(x,y,d){
-scale(1.2);
-ellipse(x,y,d);
-if(d>2){
 
-  drawCircle(x-(d*0.5),y,d-(d*0.5));
-    drawCircle(x+(d*0.5),y,d-(d*0.5));
-        //drawCircle(x,y+(d*0.5),d-(d*0.5));
-          //drawCircle(x,y-(d*0.5),d-(d*0.5));
 }
+function drawCircle(x,y,d){ 
+   ellipse(x,y,d);
+   if(d>2){
+
+     drawCircle(x-(d*0.5),y,d*0.5);
+     drawCircle(x+(d*0.5),y,d*0.5);
+     drawCircle(x,y+(d*0.5),d*0.5);
+     //drawCircle(x,y-(d*0.5),(d*0.5));
+   }
+}
+
+function sleep(delay) {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
 }
