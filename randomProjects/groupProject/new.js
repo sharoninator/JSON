74700fa674fd8c06d36feb3309752c9148a4ef57
Gorgamite <35180531+Gorgamite@users.noreply.github.com>
2018-01-21 17:09:01 -0800

    Update new.js

--- randomProjects/groupProject/new.js
+++ randomProjects/groupProject/new.js
@@ -2,50 +2,71 @@ var ball1;
 var left;
 var right;
 var isRight;
+var score = 0;
+var diff;
+var range;
+      var newRightLoc = 250;
+//float angle;
 function setup(){
   createCanvas(600,600);
-   ball1 = new Ball();
-   left = new Paddle();
-right = new Paddle();
+  ball1 = new Ball();
+  left = new Paddle(300);
+  right = new Paddle(300);
 }
 
 function draw(){
-ball1.move();
-
+  ball1.move();
   ball1.bounce();
-left.update(0,mouseY,10,100, false);
-right.update(590, ball1.y-50, 10,100 , true);
- 
+ball1.lose();
+  left.update(0,mouseY,false); //user paddle
+  right.update(590, newRightLoc,  true); // computer paddle
+       right.rightY();
+
+  textSize(25);
+  text(score.toString(),width/2,25 );
 }
 
 function Ball(){
-this.xSpeed = 7;
-this.ySpeed = 7;
+  this.xSpeed = 7;
+  this.ySpeed = -3;
   this.x = 200;
   this.y = 200;
   this.d = 30;
   this.bounce = function(){ // bounce off paddle
     //x + r > p.x - p.w/2 && y < p.y + p.h/2 && y > p.y - p.h/2
-    if(this.x + this.d / 2 >= right.x - right.width / 2 &&
+    if(this.x + this.d / 2 >= right.x - right.width / 2 && // collision for right paddle
        this.y <right.y + right.height &&
        this.y> right.y - right.height ){
-    console.log("beeep");
-    this.xSpeed = this.xSpeed * -1;
+if( right.y - ball1.y >0){
+ball1.lose();
+
+} else{
+
+       this.xSpeed = this.xSpeed * -1;
+       var hit = this.y - right.y; // hit range between 0 and height of paddle
+        range =map(hit, 0, right.height, -7,7);
+        this.ySpeed = range;
+      }
+
     }
-      else if(this.x - this.d / 2 <= left.x + left.width / 2 &&
+    else if(this.x - this.d / 2 <= left.x + left.width / 2 &&
          this.y <left.y + left.height &&
          this.y> left.y - left.height /2){
-    
-    this.xSpeed = this.xSpeed * -1;
+            this.xSpeed = this.xSpeed * -1;
+            score++;
+            // var a = 1;
+            // var b = 5;
+            // map(this.y, left.y + left.height/2, left)
+
+            var hit = this.y - left.y; // hit range between 0 and height of paddle
+             range =map(hit, 0, left.height, -7,7);
+             this.ySpeed = range;
+
     }
-console.log(this.y);
+
   }
-this.move = function(){
-        if (this.x < 0 || this.x > width) {
-this.x = random(0, width);
-          this.y = random(0,height);
-        
-        }
+  this.move = function(){
+
         if (this.y < 0 || this.y > height) {
             this.ySpeed = this.ySpeed * -1;
 
@@ -55,33 +76,47 @@ this.x = random(0, width);
 
         noStroke();
         fill(255);
-    background(51);
+        background(51);
         ellipse(this.x, this.y, this.d);
+      }
+      this.lose = function(){
+        if (this.x < 0 || this.x > width) {
+          this.x = 300;//random(0, width);
+          this.y = 300;//random(0,height);
+          score = 0;
+          this.ySpeed = 0;
+
+        }
+      }
 }
-        
-    }
-    
-    
-    function Paddle(){
-      
-      this.update = function(x,y,width,height, isRight){
+
+
+    function Paddle(y){
+      this.yLoc = y;
+
+      this.update = function(x,y, isRight){
               this.x = x;
+              this.width = 10;
+              this.height = 100;
+              this.y = y;// - height/2;
 
-this.width = width;
-this.height = height;
-this.y = y;// - height/2;
-if(this.y > 600 - this.height){
-  this.y = 600 - this.height;
-  console.log(right.y);
-}
-if(isRight){
 
-}
-
+              if(this.y > 600 - this.height){
+                this.y = 600 - this.height;
+              }
+              if(this.y < 0 ){
+                this.y =0;
+              }
         fill(175);
         noStroke();
         rect(this.x, this.y,this.width,this.height);
-      
     }
+    this.rightY = function(){
+      rightDist = this.y - ball1.y +50;
+      if(rightDist < 0){
+         newRightLoc = this.y  + 4;
+      } else{
+        newRightLoc = this.y  - 4;
+      }
     }
-  
+  }
