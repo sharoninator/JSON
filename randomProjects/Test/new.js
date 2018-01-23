964da017e9bd3ec5b44d29d2760e23fd1441e6e0
Gorgamite <35180531+Gorgamite@users.noreply.github.com>
2018-01-22 19:39:33 -0800

    Update new.js

--- randomProjects/Test/new.js
+++ randomProjects/Test/new.js
@@ -1,71 +1,59 @@
-var bird;
-var bg = true;
-var num = parseFloat(prompt("Enter number of balls: "));
-var birds = [];
-
+  "use strict";
+var ball = [];
+var mag;
+var num = parseFloat(prompt("Enter number of balls "));
 function setup() {
-    createCanvas(600, 600);
-    for (var i = 0; i < num; i++) { 
-        birds["bird" + i] = new Bird(random(0, width), random(0, height), i);
-    }
-    var button = createButton("Background");
-button.mousePressed(toggleBackground);
+   mag = createSlider(0,20,2,0.01);
+createCanvas(600,600);
+ for(var i=1;i<=num;i++){
+   ball[i] = new Ball(random(600),random(600));
+ }
 }
-function toggleBackground(){
-  if (!bg){
-    bg = true;
-  } else{
-    bg = false;
-  }
-}
+
 function draw() {
-  if(bg == true){
-    background(255);
-  } 
-    for (var j = 0; j < num; j++) { 
-        birds["bird" + j].moveBall();
-    }
+ for(var i=1;i<=num;i++){
+ball[i].drawBall(random(-mag.value(),Math.abs(mag.value())),random(-mag.value(),Math.abs(mag.value())));
+ball[i].edges();
+ }
 }
 
-//Bird object //
-function Bird(x, y, i) {
-    this.ySpeed = random(0, 1);
-    this.xSpeed = random(0, 1);
-    this.x = x;
-    this.y = y;
-    this.index = i;
-    this.d = 25;
-    this.color = {
-        r: random(255),
-        g: random(255),
-        b: random(255),
-    };
-    this.moveBall = function(x, y) {
-        if (this.ySpeed > 0.5) {
-            this.ySpeed = 5;
-        } else {
-            this.ySpeed = -5;
-        }
-        if (this.xSpeed > 0.5) {
-            this.xSpeed = 5;
-        } else {
-            this.xSpeed = -5;
-        }
 
-        if (this.x < 0 || this.x > width) {
-            this.xSpeed = this.xSpeed * -1;
-            //console.log(this.xSpeed + " " + this.index);            
-        }
-        if (this.y < 0 || this.y > height) {
-            this.ySpeed = this.ySpeed * -1;
-            //console.log(this.ySpeed + " " + this.index);
-        }
-        this.x += this.xSpeed;
-        this.y += this.ySpeed;
-        fill(this.color.r, this.color.g, this.color.b);
-        noStroke();
-        ellipse(this.x, this.y, this.d);
-        
+
+class Ball{
+  constructor(x,y){
+this.x = x;
+this.y = y;
+this.d = 30;
+
+this.colors = {
+r: random(255),
+  g:random(255),
+  b: random(255)
+};    
+  }
+  drawBall(xSpeed,ySpeed){
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+    this.x+=xSpeed;
+    this.y+=ySpeed;
+    fill(this.colors.r,this.colors.g,this.colors.b);
+    noStroke();
+    ellipse(this.x, this.y,this.d);
+    
+  }
+  edges(){
+    if(this.x > width){
+      this.x = width - this.d/2;
+    } 
+    if(this.x < 0){
+    this.x = this.d/2;
+    } 
+    if(this.y > height){
+      this.y = height
     }
-
+    if(this.y <0){
+      this.y = this.d/2;
+    }
+  }
+  
 }
